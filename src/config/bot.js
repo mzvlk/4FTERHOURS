3e663f76af327f943ebc2fe342c294e54aa3f425
mzvlk <inigomalvarez@gmail.com>
2026-05-07 21:29:04 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#DE1212", 
+      secondary: "#16171A", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
